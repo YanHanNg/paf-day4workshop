eff2310128b5e82cdb6257a301e848c3a9e3ffaa
ngyanhan <yanhanng89@gmail.com>
2020-12-11 09:03:22 +0800

    upd

--- backend/main.js
+++ backend/main.js
@@ -41,11 +41,12 @@ const upload = multer({
                 originalFilename: file.originalname,
                 uploadDatetime: new Date().toString(),
                 uploader: req.body.uploader ? req.body.uploader : req.query.uploader,
-                note: req.body.note ? req.body.note : req.query.note
+                note: req.body.note ? req.body.note : req.query.note,
+                type: req.body.type ? req.body.type : '',
+                size: req.body.size ? req.body.size : ''
             })
         },
         key: function(request, file, cb) {
-            console.log(file);
             cb(null, new Date().getTime() + '_' + file.originalname )
         }
     })
@@ -79,6 +80,47 @@ app.post('/upload', (req, res)=> {
 //         res.status(200).json({ message: 'uploaded' });
 //     })
 // })
+function encode(data){
+    let buf = Buffer.from(data);
+    let base64 = buf.toString('base64');
+    return base64
+}
+
+async function downloadFromS3(params, res) {
+    const metaData = await s3.headObject(params).promise();
+
+    let mimes = {
+        'jpeg': 'data:image/jpeg;base64,'
+    };
+
+    res.set({
+        'X-Original-Name': metaData.Metadata.originalfilename,
+        'X-Create-Time': metaData.Metadata.uploadDatetime,
+        'X-Uploader': metaData.Metadata.uploader,
+        'X-Notes': metaData.Metadata.note
+    })
+    s3.getObject(params, (err, data) => {
+        if(err)
+            console.log(err, err.stack);
+        let result = 'data:image/jpeg;base64,' + encode(data.Body);
+        // let image="<img src='data:image/jpeg;base64," + encode(data.Body) + "'" + "/>";
+        // let startHTML="<html><body></body>";
+        // let endHTML="</body></html>";
+        // let html=startHTML + image + endHTML;
+        // res.send(html)
+        res.status(200).json({ res_image: result });
+    })
+}
+
+app.get('/blob/:key', (req, res) => {
+    const params = {
+        Bucket: AWS_S3_BUCKETNAME,
+        Key: req.params.key
+    }
+
+    //Download from S3
+    downloadFromS3(params, res);
+})
 
 app.listen(APP_PORT, ()=> {
     console.info(`Server Started on PORT ${APP_PORT} at ${new Date()}`);
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   title = 'frontend';
 
   res_image: string;
+  res_image_key : string;
 
   constructor(private fb: FormBuilder, private http: HttpClient) { }
 
@@ -27,6 +28,8 @@ export class AppComponent {
     formData.set('uploader', this.form.get('uploader').value);
     formData.set('note', this.form.get('note').value);
     formData.set('name', this.form.get('imageFileName').value);
+    formData.set('type', this.imageFile.nativeElement.files[0].type);
+    formData.set('size', this.imageFile.nativeElement.files[0].size);
     //this image-file must match the backend
     formData.set('image-file', this.imageFile.nativeElement.files[0]);
   
@@ -36,7 +39,16 @@ export class AppComponent {
       .then(data => {
         console.info(data);
         //@ts-ignore
-        this.res_image = data.res_image;
+        //this.res_image = data.res_image;
+        this.res_image_key = data.res_image_key;
+        
+        //Fetch from Server as base64
+        this.http.get(`http://localhost:3000/blob/${this.res_image_key}`)
+          .toPromise()
+          .then(data => {
+            //@ts-ignore
+            this.res_image = data.res_image;
+          })
       })
       .catch(err=> { console.error('Error', err)});
   }
